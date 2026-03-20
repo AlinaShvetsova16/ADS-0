5325cbe638205c4722792874bcf00bec66118763
AlinaShvetsova16 <alinashvetsova16@gmail.com>
2026-03-20 22:13:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	while (b != 0) {
-		int date = b;
-		b = a % b;
-		a = date;
-	}
-	return a;
+    while (b != 0) {
+        int date = b;
+        b = a % b;
+        a = date;
+    }
+    return a;
 }
